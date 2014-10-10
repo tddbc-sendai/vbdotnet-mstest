775a1da6f79f9df9a6ce00ee475b37171df5770d
Masaki Yamamoto <m-yamamoto@zk.tnes.jp.nec.com>
2014-10-10 16:51:43 +0900

    Add SampleLibrary.Test Project

---
+++ SampleLibrary.Test/SampleTest.vb
@@ -0,0 +1,10 @@
+﻿Imports Microsoft.VisualStudio.TestTools.UnitTesting
+
+<TestClass> _
+Public Class SampleTest
+    <TestMethod> _
+    Public Sub TestSay()
+        Dim sample = New Sample()
+        Assert.AreEqual("Hello TDD Boot Camp!", sample.Say())
+    End Sub
+End Class
